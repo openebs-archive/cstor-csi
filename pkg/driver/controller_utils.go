1ac52180b7753f9956404655d2b543fb9228eb89
prateekpandey14 <prateek.pandey@mayadata.io>
2020-08-05 18:37:53 +0530

    fix(create): validate create volume request access mode
    Commits adds the validation to validate the create volume
    request access mode to validate and support only single node writer
    i.e. `ReadWriteOnce` accessmode and failed for other unsupported
    modes, like `ReadWriteMany`
    
    Signed-off-by: prateekpandey14 <prateek.pandey@mayadata.io>

--- pkg/driver/controller_utils.go
+++ pkg/driver/controller_utils.go
@@ -150,6 +150,13 @@ func (cs *controller) validateVolumeCreateReq(req *csi.CreateVolumeRequest) erro
 				)
 			}
 		}
+		if mode := volcap.GetAccessMode(); mode != nil {
+			modeName := csi.VolumeCapability_AccessMode_Mode_name[int32(mode.GetMode())]
+			// we only support SINGLE_NODE_WRITER
+			if mode.GetMode() != csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER {
+				return status.Errorf(codes.InvalidArgument, "unsupported access mode: %s", modeName)
+			}
+		}
 	}
 	return nil
 }
