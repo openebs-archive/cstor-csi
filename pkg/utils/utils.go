c9a418d9aa9f90380d3c7c9a3b7da7ab45f72fa5
Payes Anand <payes.anand@mayadata.io>
2020-09-15 03:11:50 +0530

    resolve race condition in remount code
    Signed-off-by: Payes Anand <payes.anand@mayadata.io>

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -280,19 +280,19 @@ func MonitorMounts() {
 					continue
 				}
 				if _, ok := TransitionVolList[vol.Spec.Volume.Name]; !ok {
-					TransitionVolList[vol.Spec.Volume.Name] = vol.Status
-					ReqMountList[vol.Spec.Volume.Name] = vol.Status
 					csivol := vol
-					go func() {
+					TransitionVolList[csivol.Spec.Volume.Name] = csivol.Status
+					ReqMountList[csivol.Spec.Volume.Name] = csivol.Status
+					go func(csivol apis.CStorVolumeAttachment) {
 						logrus.Infof("Remounting vol: %s at %s and %s",
-							vol.Spec.Volume.Name, vol.Spec.Volume.StagingTargetPath,
-							vol.Spec.Volume.TargetPath)
+							csivol.Spec.Volume.Name, csivol.Spec.Volume.StagingTargetPath,
+							csivol.Spec.Volume.TargetPath)
 						defer func() {
 							TransitionVolListLock.Lock()
 							// Remove the volume from ReqMountList once the remount operation is
 							// complete
-							delete(TransitionVolList, vol.Spec.Volume.Name)
-							delete(ReqMountList, vol.Spec.Volume.Name)
+							delete(TransitionVolList, csivol.Spec.Volume.Name)
+							delete(ReqMountList, csivol.Spec.Volume.Name)
 							TransitionVolListLock.Unlock()
 						}()
 						if err := RemountVolume(
@@ -301,15 +301,15 @@ func MonitorMounts() {
 						); err != nil {
 							logrus.Errorf(
 								"Remount failed for vol: %s : err: %v",
-								vol.Spec.Volume.Name, err,
+								csivol.Spec.Volume.Name, err,
 							)
 						} else {
 							logrus.Infof(
 								"Remount successful for vol: %s",
-								vol.Spec.Volume.Name,
+								csivol.Spec.Volume.Name,
 							)
 						}
-					}()
+					}(csivol)
 				}
 			}
 			TransitionVolListLock.Unlock()
